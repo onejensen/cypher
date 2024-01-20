398f7970227364887ea93aec614f9198d686445d
HaoZhang <sid199057@gmail.com>
2024-01-20 21:56:44 +0200

    user experience improvement

--- cmd/main.go
+++ cmd/main.go
@@ -14,82 +14,85 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Welcome to the Cypher Tool!")
 
-	// Get the user's choice of operation (encrypt or decrypt)
-	toEncrypt, err := util.GetOperation(reader)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	// Get the user's choice of encryption type
-	encoding, err := util.GetCipherType(reader)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	// Get the user's message
-	fmt.Println("Enter the message:")
-	message, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading message:", err)
-		return
-	}
-	// Remove whitespace from both ends of the message
-	message = strings.TrimSpace(message)
-
-	// Perform encryption or decryption based on the chosen type
-	var result, operation string
-	var shift int
-	if encoding == "Caesar" {
-		fmt.Print("Enter the shift amount: ")
-		fmt.Scanf("%d\n", &shift)
-	}
-	switch encoding {
-	case "ROT13":
-		// Encrypt or decrypt the message using ROT13
-		if toEncrypt {
-			result = cipher.EncryptROT13(message)
-			operation = "Encryption"
-		} else {
-			result = cipher.DecryptROT13(message)
-			operation = "Decryption"
+	for {
+		// Get the user's choice of operation (encrypt or decrypt)
+		toEncrypt, err := util.GetOperation(reader)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
 		}
-	case "Reverse":
-		// Encrypt or decrypt the message using Reverse
-		if toEncrypt {
-			result = cipher.EncryptReverse(message)
-			operation = "Encryption"
-		} else {
-			result = cipher.DecryptReverse(message)
-			operation = "Decryption"
+
+		// Get the user's choice of encryption type or to go back to the main menu
+		encoding, backToMenu, err := util.GetCipherType(reader)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
 		}
-		// More encryption methods can be added here
-	case "Caesar":
-		// Encrypt or decrypt the message using Caesar cipher
-		if toEncrypt {
-			result = cipher.EncryptCaesar(message, shift)
-			operation = "Encryption"
-		} else {
-			result = cipher.DecryptCaesar(message, shift)
-			operation = "Decryption"
+		if backToMenu {
+			continue
 		}
-	}
 
-	// Print the result of encryption or decryption
-	fmt.Printf("%s result using %s: %s\n", operation, encoding, result)
+		// Get the user's message
+		fmt.Println("Enter the message:")
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			return
+		}
+		// Remove whitespace from both ends of the message
+		message = strings.TrimSpace(message)
 
-	// Wait for the user to press Enter to prevent the program from immediately ending
-	fmt.Println("Would you like to do another operation?")
-	toChoose, err := util.GetOption(reader)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	if toChoose {
-		main()
-	} else {
-		fmt.Println("Thank you for using our app! (Created with love by Hao, Auri & Juanjo)")
+		// Perform encryption or decryption based on the chosen type
+		var result, operation string
+		var shift int
+		if encoding == "Caesar" {
+			fmt.Print("Enter the shift amount: ")
+			fmt.Scanf("%d\n", &shift)
+		}
+		switch encoding {
+		case "ROT13":
+			if toEncrypt {
+				result = cipher.EncryptROT13(message)
+				operation = "Encryption"
+			} else {
+				result = cipher.DecryptROT13(message)
+				operation = "Decryption"
+			}
+		case "Reverse":
+			if toEncrypt {
+				result = cipher.EncryptReverse(message)
+				operation = "Encryption"
+			} else {
+				result = cipher.DecryptReverse(message)
+				operation = "Decryption"
+			}
+		case "Caesar":
+			if toEncrypt {
+				result = cipher.EncryptCaesar(message, shift)
+				operation = "Encryption"
+			} else {
+				result = cipher.DecryptCaesar(message, shift)
+				operation = "Decryption"
+			}
+		}
 
+		// Print the result of encryption or decryption
+		fmt.Printf("%s result using %s: %s\n", operation, encoding, result)
+		fmt.Println("Please save your password. Press Enter to continue...")
+
+		// Wait for the user to press Enter
+		_, _ = reader.ReadString('\n')
+
+		// Ask user if they would like to perform another operation
+		fmt.Println("Would you like to do another operation?")
+		toChoose, err := util.GetOption(reader)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		if !toChoose {
+			fmt.Println("Thank you for using our app! (Created with love by Hao, Auri & Juanjo)")
+			break
+		}
 	}
 }
--- util/util.go
+++ util/util.go
@@ -29,6 +29,33 @@ func GetOperation(reader *bufio.Reader) (bool, error) {
 	}
 }
 
+// GetCipherType repeatedly gets user input until the user selects a valid cipher type or chooses to go back
+// Input: reader - bufio.Reader object for reading from standard input
+// Output: (string, bool, error) - The first return value is the user's selected cipher type ("ROT13", "Reverse", "Caesar"). The second return value is true if the user wants to go back to the main menu. The third return value is for any possible errors.
+func GetCipherType(reader *bufio.Reader) (string, bool, error) {
+	for {
+		fmt.Printf("Select cipher (1/2/3/4):\n1. ROT13 (ONLY LATIN ALPHABET).\n2. Reverse.\n3. Caesar Cipher (ONLY LATIN ALPHABET).\n4. Back to Main Menu.\n")
+		cipher, err := reader.ReadString('\n')
+		if err != nil {
+			return "", false, err
+		}
+		cipher = strings.TrimSpace(cipher)
+
+		switch cipher {
+		case "1":
+			return "ROT13", false, nil // User chose ROT13 encryption
+		case "2":
+			return "Reverse", false, nil // User chose Reverse encryption
+		case "3":
+			return "Caesar", false, nil // User chose Caesar Cipher
+		case "4":
+			return "", true, nil // User chose to go back to the main menu
+		default:
+			fmt.Printf("Invalid cipher type, please enter 1, 2, 3, or 4.\n")
+		}
+	}
+}
+
 func GetOption(reader *bufio.Reader) (bool, error) {
 	for {
 		fmt.Printf("Select option (1/2):\n1. Yes.\n2. No.\n")
@@ -40,36 +67,11 @@ func GetOption(reader *bufio.Reader) (bool, error) {
 
 		switch opt {
 		case "1":
-			return true, nil // User chose encryption
+			return true, nil // User chose Yes
 		case "2":
-			return false, nil // User chose decryption
+			return false, nil // User chose No
 		default:
 			fmt.Printf("Invalid option, please enter 1 or 2.\n")
-		}
-	}
-}
-
-// GetCipherType repeatedly gets user input until the user selects a valid cipher type
-// Input: reader - bufio.Reader object for reading from standard input
-// Output: (string, error) - The first return value is the user's selected cipher type ("ROT13", "Reverse", "Caesar"), the second return value is for any possible errors.
-func GetCipherType(reader *bufio.Reader) (string, error) {
-	for {
-		fmt.Printf("Select cipher (1/3):\n1. ROT13 (ONLY LATIN ALPHABET).\n2. Reverse.\n3. Caesar Cipher (ONLY LATIN ALPHABET)\n")
-		cipher, err := reader.ReadString('\n')
-		if err != nil {
-			return "", err
-		}
-		cipher = strings.TrimSpace(cipher)
-
-		switch cipher {
-		case "1":
-			return "ROT13", nil // User chose ROT13 encryption
-		case "2":
-			return "Reverse", nil // User chose Reverse encryption
-		case "3":
-			return "Caesar", nil // User chose Caesar Cipher
-		default:
-			fmt.Printf("Invalid cipher type, please enter 1, 2, or 3.\n")
 		}
 	}
 }
